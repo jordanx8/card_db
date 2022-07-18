2222c35faf4b0d745a35e266f9f82c90424fbda8
jordanx8 <froggyairplane@gmail.com>
2022-07-17 21:42:26 -0500

    search bar/nested tables/images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,27 +20,55 @@ import {
 import './App.scss';
 
 function App() {
+  const images =
+  {
+    Steven_Adams: "https://www.basketball-reference.com/req/202106291/images/players/adamsst01.jpg"
+  }
+
+  const headers1 = [
+    {
+      key: 'season',
+      header: 'Season'
+    },
+  ]
+
+  const rows1 = [
+    {
+      id: 'a',
+      season: "2019-20 ( Oklahoma City Thunder )",
+    },
+    {
+      id: 'b',
+      season: "2020-21 ( New Orleans Pelicans )",
+    }
+  ]
+
   const rows2 = [
     {
       id: 'a',
       firstname: "Steven",
       lastname: "Adams",
-      seasonsplayed: "( 2020-21 )"
+      seasonsplayed: "( 2020-21 )",
+      numcards: "4"
     }
   ]
   const headers2 = [
     {
       key: 'firstname',
-      header: "First Name"
+      header: "First"
     },
     {
       key: 'lastname',
-      header: "Last Name"
+      header: "Last"
     },
     {
       key: 'seasonsplayed',
       header: "Season"
     },
+    {
+      key: 'numcards',
+      header: "Number of Cards"
+    }
   ]
   const rows3 = [
     {
@@ -65,10 +93,6 @@ function App() {
     },
   ];
   const headers3 = [
-    {
-      key: 'season',
-      header: 'Season',
-    },
     {
       key: 'manufacturer',
       header: 'Manufacturer',
@@ -99,65 +123,97 @@ function App() {
     <DataTable rows={rows2} headers={headers2}>
       {({ rows, headers, getTableProps, getHeaderProps, getRowProps, getBatchActionProps, onInputChange }) => (
         <>
-        <TableToolbar>
-        <TableToolbarContent>
-          <TableToolbarSearch
-            tabIndex={getBatchActionProps().shouldShowBatchActions ? -1 : 0}
-            onChange={onInputChange}
-          />
-        </TableToolbarContent>
-      </TableToolbar>
-        <Table {...getTableProps()}>
-          <TableHead>
-            <TableRow>
-              <TableExpandHeader></TableExpandHeader>
-              {headers.map((header) => (
-                <TableHeader  {...getHeaderProps({ header, isSortable: true })}>
-                  {header.header}
-                </TableHeader >
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <>
-                <TableExpandRow {...getRowProps({ row })}>
-                  {row.cells.map((cell) => (
-                    <TableCell key={cell.id}>{cell.value}</TableCell>
-                  ))}
-                </TableExpandRow>
-                {row.isExpanded && (
-                  <TableExpandedRow colSpan={headers.length + 1}>
-                    <DataTable rows={rows3} headers={headers3}>
-                      {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
-                        <Table {...getTableProps()}>
-                          <TableHead>
-                            <TableRow>
-                              {headers.map((header) => (
-                                <TableHeader  {...getHeaderProps({ header, isSortable: true })}>
-                                  {header.header}
-                                </TableHeader >
+          <TableToolbar>
+            <TableToolbarContent>
+              <TableToolbarSearch
+                tabIndex={getBatchActionProps().shouldShowBatchActions ? -1 : 0}
+                onChange={onInputChange}
+              />
+            </TableToolbarContent>
+          </TableToolbar>
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                <TableExpandHeader></TableExpandHeader>
+                <TableHeader></TableHeader>
+                {headers.map((header) => (
+                  <TableHeader  {...getHeaderProps({ header, isSortable: true })}>
+                    {header.header}
+                  </TableHeader >
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <>
+                  <TableExpandRow {...getRowProps({ row })}>
+                    <TableCell><img src={images[row.cells[0].value + '_' + row.cells[1].value]} alt={row.cells[0].value + ' ' + row.cells[1].value}></img></TableCell>
+                    {row.cells.map((cell) => (
+                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                    ))}
+                  </TableExpandRow>
+                  {row.isExpanded && (
+                    <TableExpandedRow colSpan={headers.length + 2}>
+                      <DataTable rows={rows1} headers={headers1}>
+                        {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+                          <Table {...getTableProps()}>
+                            <TableHead>
+                              <TableRow>
+                                {headers.map((header) => (
+                                  <TableHeader colSpan={headers.length + 1}  {...getHeaderProps({ header, isSortable: true })}>
+                                    {header.header}
+                                  </TableHeader >
+                                ))}
+                              </TableRow>
+                            </TableHead>
+                            <TableBody>
+                              {rows.map((row) => (
+                                <>
+                                  <TableExpandRow {...getRowProps({ row })}>
+                                    {row.cells.map((cell) => (
+                                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                                    ))}
+                                  </TableExpandRow>
+                                  {row.isExpanded && (
+                                    <TableExpandedRow colSpan={headers.length + 2}>
+                                      <DataTable rows={rows3} headers={headers3}>
+                                        {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+                                          <Table {...getTableProps()}>
+                                            <TableHead>
+                                              <TableRow>
+                                                {headers.map((header) => (
+                                                  <TableHeader  {...getHeaderProps({ header, isSortable: true })}>
+                                                    {header.header}
+                                                  </TableHeader >
+                                                ))}
+                                              </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                              {rows.map((row) => (
+                                                <TableRow {...getRowProps({ row })}>
+                                                  {row.cells.map((cell) => (
+                                                    <TableCell key={cell.id}>{cell.value}</TableCell>
+                                                  ))}
+                                                </TableRow>
+                                              ))}
+                                            </TableBody>
+                                          </Table>
+                                        )}
+                                      </DataTable>
+                                    </TableExpandedRow>
+                                  )}
+                                </>
                               ))}
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {rows.map((row) => (
-                                <TableRow {...getRowProps({ row })}>
-                                  {row.cells.map((cell) => (
-                                    <TableCell key={cell.id}>{cell.value}</TableCell>
-                                  ))}
-                                </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      )}
-                    </DataTable>
-                  </TableExpandedRow>
-                )}
-              </>
-            ))}
-          </TableBody>
-        </Table>
+                            </TableBody>
+                          </Table>
+                        )}
+                      </DataTable>
+                    </TableExpandedRow>
+                  )}
+                </>
+              ))}
+            </TableBody>
+          </Table>
         </>
       )}
     </DataTable>
