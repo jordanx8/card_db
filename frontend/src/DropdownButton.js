d84190f24490aca04ee79112e7fc7e1a6701aacf
jordanx8 <froggyairplane@gmail.com>
2022-11-10 18:59:34 -0600

    react-bootstrap-icons

--- frontend/src/DropdownButton.js
+++ frontend/src/DropdownButton.js
@@ -1,11 +1,21 @@
+import { ArrowUp, ArrowDown } from 'react-bootstrap-icons';
+
 function DropdownButton(props) {
     function useStateHandler(){
         props.setState(!props.state)
     }
 
-    return (
-        <button onClick={useStateHandler}>^</button>
-    );
+    if(!props.state)
+    {
+        return (
+            <button onClick={useStateHandler}><ArrowUp/></button>
+        );
+    }else{
+        return (
+            <button onClick={useStateHandler}><ArrowDown/></button>
+        );
+    }
+
 }
 
 export default DropdownButton;
